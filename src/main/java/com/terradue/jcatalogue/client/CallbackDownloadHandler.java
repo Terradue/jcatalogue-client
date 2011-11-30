f0dac28d17342f2b130bc9c38d3cf612474e9cf4
simonetripodi <simone.tripodi@gmail.com>
2011-11-30 12:28:07 +0100

    added a DownloadHandler adapter to implement callbacks

---
+++ src/main/java/com/terradue/jcatalogue/client/CallbackDownloadHandler.java
@@ -0,0 +1,39 @@
+package com.terradue.jcatalogue.client;
+
+import java.io.File;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import com.terradue.jcatalogue.client.download.DownloadHandler;
+
+@RequiredArgsConstructor
+final class CallbackDownloadHandler
+    implements DownloadHandler
+{
+
+    private final DownloadHandler adapted;
+
+    @Getter
+    private boolean downloaded = false;
+
+    @Override
+    public void onError( Throwable t )
+    {
+        adapted.onError( t );
+    }
+
+    @Override
+    public void onError( String message )
+    {
+        adapted.onError( message );
+    }
+
+    @Override
+    public void onSuccess( File file )
+    {
+        downloaded = true;
+        adapted.onSuccess( file );
+    }
+
+}
