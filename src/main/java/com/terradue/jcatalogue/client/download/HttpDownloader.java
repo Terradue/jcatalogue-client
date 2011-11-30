771fa3ac29a6ab6be7abce9ce56c407d779db412
simonetripodi <simone.tripodi@gmail.com>
2011-11-30 09:30:51 +0100

    no automatic discover of downloader implementations

--- src/main/java/com/terradue/jcatalogue/client/download/HttpDownloader.java
+++ src/main/java/com/terradue/jcatalogue/client/download/HttpDownloader.java
@@ -2,9 +2,6 @@ package com.terradue.jcatalogue.client.download;
 
 import java.io.File;
 
-import org.kohsuke.MetaInfServices;
-
-@MetaInfServices
 @Protocol({ "http", "https" })
 final class HttpDownloader
     implements Downloader
