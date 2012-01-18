5595d4621df3d451a5e5a1313071faaa1607ccf6
simonetripodi <simone.tripodi@gmail.com>
2012-01-18 17:19:24 +0100

    replaced the download handler

--- src/main/java/com/terradue/jcatalogue/client/download/HttpDownloader.java
+++ src/main/java/com/terradue/jcatalogue/client/download/HttpDownloader.java
@@ -1,36 +1,26 @@
 package com.terradue.jcatalogue.client.download;
 
-import static java.lang.String.format;
-import static java.net.HttpURLConnection.HTTP_OK;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.Realm;
 import com.ning.http.client.RequestBuilder;
-import com.ning.http.client.Response;
-import com.ning.http.client.extra.ResumableRandomAccessFileListener;
-import com.ning.http.client.resumable.ResumableAsyncHandler;
 
 @Data
 @Protocol({ "http", "https" })
 public final class HttpDownloader
     implements Downloader
 {
-
-    private static final String RW = "rw";
 
     private static final String GET = "GET";
 
@@ -67,17 +57,6 @@ public final class HttpDownloader
             logger.info( "Downloading {} to {}...", fileUri, targetDir );
         }
 
-        ResumableAsyncHandler<Response> resumableHandler = new ResumableAsyncHandler<Response>();
-        try
-        {
-            resumableHandler.setResumableListener( new ResumableRandomAccessFileListener( new RandomAccessFile( targetFile,
-                                                                                                                RW ) ) );
-        }
-        catch ( FileNotFoundException e )
-        {
-            // TODO verify, but that should not happen
-        }
-
         RequestBuilder requestBuilder = new RequestBuilder( GET ).setUrl( fileUri.toString() );
 
         if ( realms.containsKey( fileUri.getHost() ) )
@@ -85,21 +64,9 @@ public final class HttpDownloader
             requestBuilder.setRealm( realms.get( fileUri.getHost() ) );
         }
 
-        Response response;
         try
         {
-            response = httpClient.executeRequest( requestBuilder.build(), resumableHandler ).get();
-
-            if ( HTTP_OK != response.getStatusCode() )
-            {
-                handler.onError( format( "Impossible to download %s from %s, server replied %s",
-                                         targetFile.getName(), fileUri, response.getStatusText() ) ) ;
-                targetFile.delete();
-            }
-            else
-            {
-                handler.onSuccess( targetFile );
-            }
+            httpClient.executeRequest( requestBuilder.build(), new SimpleDownloadHanlder( targetFile, handler ) ).get();
         }
         catch ( Exception e )
         {
