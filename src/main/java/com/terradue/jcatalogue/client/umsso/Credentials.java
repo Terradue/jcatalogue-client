58a353e4b7bb04b784cbb30dfcdaa1ae66e3a3fe
simonetripodi <simone.tripodi@gmail.com>
2012-02-21 09:23:55 +0100

    first checkin of UM-SSO Credentials stuff

---
+++ src/main/java/com/terradue/jcatalogue/client/umsso/Credentials.java
@@ -0,0 +1,16 @@
+package com.terradue.jcatalogue.client.umsso;
+
+import lombok.Data;
+
+/**
+ * @since 0.8
+ */
+@Data
+public final class Credentials
+{
+
+    private final String username;
+
+    private final String password;
+
+}
