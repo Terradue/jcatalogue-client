3a0c7628417ec11d2bd906f8d505aac35e696754
simonetripodi <simone.tripodi@gmail.com>
2012-02-23 11:47:56 +0100

    updated license header

--- src/main/java/com/terradue/jcatalogue/client/geo/Box.java
+++ src/main/java/com/terradue/jcatalogue/client/geo/Box.java
@@ -1,7 +1,7 @@
 package com.terradue.jcatalogue.client.geo;
 
 /*
- *    Copyright 2011 Terradue srl
+ *    Copyright 2011-2012 Terradue srl
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
