b93a85500a6797de8200ba22f0da9907b010fd90
simonetripodi <simone.tripodi@gmail.com>
2012-04-11 10:33:07 +0200

    started including the implementation module

---
+++ impl/src/main/java/com/terradue/jcatalogue/DefaultCatalogueSystem.java
@@ -0,0 +1,19 @@
+package com.terradue.jcatalogue;
+
+import com.terradue.jcatalogue.metadata.CatalogueDescription;
+import com.terradue.jcatalogue.transfer.MetadataNotFoundException;
+import com.terradue.jcatalogue.transfer.MetadataTransferException;
+
+public final class DefaultCatalogueSystem
+    implements CatalogueSystem
+{
+
+    @Override
+    public CatalogueDescription resolveDescription( CatalogueSystemSession session )
+        throws MetadataNotFoundException, MetadataTransferException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
