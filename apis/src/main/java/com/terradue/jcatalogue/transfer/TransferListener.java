04a8ddb0e0b1670d232b305e6d28c931ba3d91b9
simonetripodi <simone.tripodi@gmail.com>
2012-06-12 23:10:40 +0200

    just added ALv2 header

--- apis/src/main/java/com/terradue/jcatalogue/transfer/TransferListener.java
+++ apis/src/main/java/com/terradue/jcatalogue/transfer/TransferListener.java
@@ -1,5 +1,21 @@
 package com.terradue.jcatalogue.transfer;
 
+/*
+ *    Copyright 2011-2012 Terradue srl
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 /**
  * A listener being notified of artifact/metadata transfers from/to remote repositories. The listener may be called from
  * an arbitrary thread. Reusing common regular expression syntax, the sequence of events is roughly as follows:
