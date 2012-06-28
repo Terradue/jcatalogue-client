e1819f6918b47b4162b67b90ec039b357a6d464c
simonetripodi <simone.tripodi@gmail.com>
2012-06-28 15:49:49 +0200

    no more checksum calculation

--- core/src/main/java/com/terradue/jcatalogue/client/download/HttpDownloadHandler.java
+++ core/src/main/java/com/terradue/jcatalogue/client/download/HttpDownloadHandler.java
@@ -1,19 +1,10 @@
 package com.terradue.jcatalogue.client.download;
 
-import static java.nio.charset.Charset.forName;
-import static java.lang.String.format;
-import static java.security.MessageDigest.getInstance;
-
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import com.ning.http.client.AsyncHandler;
@@ -28,24 +19,13 @@ final class HttpDownloadHandler<T>
     implements AsyncHandler<T>
 {
 
-    /**
-     * Used to build output as Hex
-     */
-    private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-
-    private static final Charset UTF_8 = forName( "UTF-8" );
-
     private static final String CONTENT_LENGTH = "Content-Length";
-
-    private static final String MD5 = "MD5";
 
     private final File targetFile;
 
     private final FileOutputStream output;
 
     private final DownloadHandler<T> downloadHandler;
-
-    private final MessageDigest md5Digest;
 
     private long contentLength = -1;
 
@@ -57,16 +37,6 @@ final class HttpDownloadHandler<T>
         this.targetFile = targetFile;
         output = new FileOutputStream( targetFile );
         this.downloadHandler = downloadHandler;
-
-        try
-        {
-            md5Digest = getInstance( MD5 );
-        }
-        catch ( NoSuchAlgorithmException e )
-        {
-            // should not happen
-            throw new RuntimeException( e.getMessage() );
-        }
     }
 
     @Override
@@ -80,7 +50,6 @@ final class HttpDownloadHandler<T>
         throws Exception
     {
         bodyPart.writeTo( output );
-        md5Digest.update( bodyPart.getBodyPartBytes() );
 
         // print the percentage progress on shell
         if ( contentLength > 0 )
@@ -121,35 +90,7 @@ final class HttpDownloadHandler<T>
         output.flush();
         closeQuietly( output );
 
-        writeMd5ChecksumFile();
-
         return downloadHandler.onCompleted( targetFile );
-    }
-
-    private void writeMd5ChecksumFile()
-    {
-        File checksumFile = new File( targetFile.getParent(), format( "%s.md5", targetFile.getName() ) );
-        try
-        {
-            FileChannel digestChannel = new FileOutputStream( checksumFile ).getChannel();
-
-            byte[] checksumData = md5Digest.digest();
-            char[] hexData = new char[checksumData.length << 1];
-
-            for ( int i = 0, j = 0; i < checksumData.length; i++ )
-            {
-                hexData[j++] = HEX_DIGITS[( 0xF0 & hexData[i] ) >>> 4];
-                hexData[j++] = HEX_DIGITS[0x0F & hexData[i]];
-            }
-
-            digestChannel.write( ByteBuffer.wrap( new String( hexData ).getBytes( UTF_8 ) ) );
-
-            digestChannel.close();
-        }
-        catch ( Exception e )
-        {
-            // simply skip the .md5 file generation
-        }
     }
 
     private static void closeQuietly( Closeable closeable )
